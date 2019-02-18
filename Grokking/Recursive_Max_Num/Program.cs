d93329bd0b278c278ef0c74f8941656a6c185fbb
bdeguzis <b.deguzis@gmail.com>
2019-02-18 15:05:07 -0500

    Recursive Max Num

---
+++ Grokking/Recursive_Max_Num/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_Max_Num
+{
+    class Program
+    {
+
+        //Recursively find the max number in a list
+
+        static int max = 0;
+        static int maxNum (int[] nums)
+        {
+            if (nums.Length == 0)
+                return max;
+            else
+            {
+
+                if (nums[0] > max)
+                    max = nums[0];
+
+                int[] newArray = nums.Skip(1).Take(nums.Length).ToArray();
+                return maxNum(newArray);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 10, 4, 5 };
+            Console.WriteLine(maxNum(nums));
+            Console.ReadLine();
+        }
+    }
+}
